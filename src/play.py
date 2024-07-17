1f6010937cb813c57d9ae479615f0e0b763a24c1
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-07-17 12:46:20 +0800

    formatter: code style
    isort .
    black * -l 79

--- src/play.py
+++ src/play.py
@@ -1,9 +1,11 @@
-from error_process import max_row, max_column, restore_times, win_condition
-from player import player1
 import random
+
+from error_process import max_column, max_row, restore_times, win_condition
+from player import player1
 
 choice_value = (2, 4, 8, 16, 32)
 weight = (0.6, 0.25, 0.1, 0.04, 0.01)
+
 
 class CircularIteratorIndex:
     def __init__(self) -> None:
@@ -20,6 +22,7 @@ class CircularIteratorIndex:
     def __current__(self):
         return self.index
 
+
 index_iterator = CircularIteratorIndex()
 # index_iterator.__next__()
 # print(index_iterator.index)
@@ -27,12 +30,11 @@ index_iterator = CircularIteratorIndex()
 
 class Numbers:
     def __init__(self) -> None:
-
         self._data_dict = {}
         self._score_dict = {}
         for i in range(restore_times):
             self._data_dict[i] = []
-            self._score_dict [i] = 0
+            self._score_dict[i] = 0
 
         for x in range(max_row):
             row = []
@@ -45,26 +47,26 @@ class Numbers:
 
         def none_checker(data):
             index = []
-            for index_row, item in enumerate (data):
+            for index_row, item in enumerate(data):
                 for index_column, item2 in enumerate(item):
                     if item2 == None:
-                        index.append([index_row,index_column])
+                        index.append([index_row, index_column])
             return index
-        
-        def random_indenx_with_split(rand_number,check_idenx):
+
+        def random_indenx_with_split(rand_number, check_idenx):
             if rand_number == 2:
-                if random.choices((1,2),weights=(0.6,0.4),k=1)[0] == 2:
+                if random.choices((1, 2), weights=(0.6, 0.4), k=1)[0] == 2:
                     nonlocal split
-                    rand_index_list = random.choices(check_idenx,k=2)
+                    rand_index_list = random.choices(check_idenx, k=2)
                     split = True
                     return rand_index_list
-                else :
+                else:
                     rand_index = random.choice(check_index)
                     return rand_index
             else:
                 rand_index = random.choice(check_index)
                 return rand_index
-                        
+
         current_index = index_iterator.__current__()
         check_index = none_checker(self._data_dict[current_index])
         if check_index:
@@ -72,11 +74,17 @@ class Numbers:
             rand_index = random_indenx_with_split(rand_number, check_index)
             # print (split)
             if split == True:
-                rand_index1,rand_index2 = rand_index
-                self._data_dict[current_index][rand_index1[0]][rand_index1[1]] = rand_number
-                self._data_dict[current_index][rand_index2[0]][rand_index2[1]] = rand_number
+                rand_index1, rand_index2 = rand_index
+                self._data_dict[current_index][rand_index1[0]][
+                    rand_index1[1]
+                ] = rand_number
+                self._data_dict[current_index][rand_index2[0]][
+                    rand_index2[1]
+                ] = rand_number
             else:
-                self._data_dict[current_index][rand_index[0]][rand_index[1]] = rand_number
+                self._data_dict[current_index][rand_index[0]][
+                    rand_index[1]
+                ] = rand_number
         else:
             player1.win = False
 
@@ -103,7 +111,7 @@ class Numbers:
                 temp_number = item2
                 temp_index += 1
         return temp_row
-    
+
     def score(self, index):
         self._score_dict[index] = player1.score
 
@@ -134,7 +142,8 @@ class Numbers:
         ]
         temp_row_list = list(map(self.moving, matrix))
         matrix = [
-            [row[i] for row in temp_row_list] for i in range(len(temp_row_list[0]))
+            [row[i] for row in temp_row_list]
+            for i in range(len(temp_row_list[0]))
         ]
         current_index_a = index_iterator.__next__()
         self._data_dict[current_index_a] = matrix
@@ -151,21 +160,22 @@ class Numbers:
         current_index_a = index_iterator.__next__()
         self._data_dict[current_index_a] = matrix
         self.score(current_index_a)
-    
+
     def reverse(self):
         current_index = index_iterator.__previous__()
         player1.score = self._score_dict[current_index]
-    
+
     def get_current_data(self):
         current_index = index_iterator.__current__()
         current_data = self._data_dict[current_index]
         return current_data
-    
+
     def get_current_score(self):
         current_index = index_iterator.__current__()
         current_score = self._score_dict[current_index]
         return current_score
-    
+
+
 playing = Numbers()
 # generater_read = Numbers()
 
--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,7 @@
-#basic settings can be changed here 
+# basic settings can be changed here
 
-username = 'username'
+username = "username"
 max_column = 5
 max_row = 5
 restore_times = 4
 win_condition = 2048
-
