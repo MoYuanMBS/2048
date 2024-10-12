c590d6c3bfed1033c7f30e5b0497f6de3a2dfe10
MoYuanMBS <jiang04071998@hotmail.com>
2024-10-12 01:27:28 -0400

    win_loose_check_function_separate_rewrite

--- player.py
+++ player.py
@@ -1,12 +1,15 @@
 from error_process import username
+from enum import Enum
 
 class Player():
     def __init__(self) -> None:
         self.name = username
         self.score = 0
-        self.win = 0
+        self.win:Player.State = Player.State.Nonee
         self.playtimes = 0
-player1 = Player()
-
-
+    class State(Enum):
+        Win = 1
+        Loose = -1
+        Nonee = 0
     
+player1 = Player()
