82f8de13be263b9426d273a868097c1d6efb8a43
MoYuanMBS <jiang04071998@hotmail.com>
2024-10-12 01:27:28 -0400

    win_loose_check_function_separate_rewrite

--- mainfile.py
+++ mainfile.py
@@ -1,26 +1,30 @@
-from player import player1
+from player import player1,Player
 from play import playing
 from generater import generater
 from error_process import user_input
 
-# while player1.win is False:
-#     playing.random_generator()
-
 user_input.checked_valu_return()
 
-def user_state (func):
-        func()
-        if player1.win is True:
-            generater.print_full_grid
-            generater.print_win()
-            return
-        elif player1.win is False:
-            generater.print_full_grid
-            generater.print_loose()
+def user_state (func) -> None:
+        move_available = func()
+        if move_available is True:
+            playing.random_generator()
+            playing.win_lose_moving_checking()
+            print (player1.win)
+            if player1.win is Player.State.Win:
+                generater.print_full_grid()
+                generater.print_win()
+                return
+            if player1.win is Player.State.Loose:
+                generater.print_full_grid()
+                generater.print_loose()
+                return
+            generater.print_full_grid()
             return
         else:
             pass
-            return
+        return
+
 def user_playing ():
     result = user_input.user_input()
     imput_set = {'A','W','S','D'}
@@ -33,8 +37,6 @@ def user_playing ():
             user_state(playing.moving_up)
         elif result == 'S':
             user_state(playing.moving_down)
-        playing.random_generator()
-        generater.print_full_grid()
     else:
         if result == 'R':
             playing.reverse()
@@ -43,7 +45,7 @@ def user_playing ():
 def one_time_playing ():
     playing.random_generator()
     generater.print_full_grid()
-    while player1.win == 0:
+    while player1.win is Player.State.Nonee:
         user_playing()
     player1.playtimes += 1
 
@@ -52,7 +54,11 @@ if player1.playtimes == 0:
 else:
     restart_input = user_input.user_input()
     if user_input == 'R':
+        temp_playtimes = player1.playtimes
+        playing.reset_games()
         one_time_playing()
+        player1.playtimes = temp_playtimes + 1
+
 # playing.random_generator()
 # c = playing.get_current_data()
 # generater.print_full_grid() 
--- play.py
+++ play.py
@@ -1,5 +1,5 @@
 from error_process import max_row, max_column, restore_times, win_condition
-from player import player1
+from player import player1, Player
 import random
 
 choice_value = (2, 4, 8, 16, 32)
@@ -78,7 +78,7 @@ class Numbers:
             else:
                 self._data_dict[current_index][rand_index[0]][rand_index[1]] = rand_number
         else:
-            player1.win = False
+            pass
 
     def moving(self, temp_row):
         temp_index = 0
@@ -97,7 +97,7 @@ class Numbers:
                     player1.score += item3
                     already_added = True
                     if item3 == win_condition:
-                        player1.win = True
+                        player1.win = Player.State.Win
                 else:
                     already_added = False
                 temp_number = item2
@@ -107,14 +107,19 @@ class Numbers:
     def score(self, index):
         self._score_dict[index] = player1.score
 
-    def moving_left(self):
+    def moving_left(self) -> bool:
         current_index = index_iterator.__current__()
-        temp_row_list = list(map(self.moving, self._data_dict[current_index]))
-        current_index_a = index_iterator.__next__()
-        self._data_dict[current_index_a] = temp_row_list
-        self.score(current_index_a)
+        movealbe_check_result = set(map(lambda item: self.moveable_and_Loose_checker(item)[0], self._data_dict[current_index]))
+        print(movealbe_check_result)
+        if True in movealbe_check_result:
+            temp_row_list = list(map(self.moving, self._data_dict[current_index]))
+            current_index_a = index_iterator.__next__()
+            self._data_dict[current_index_a] = temp_row_list
+            self.score(current_index_a)
+            return True
+        return False
 
-    def moving_right(self):
+    def moving_right(self) -> bool:
         current_index = index_iterator.__current__()
         temp_row_list = list(
             map(
@@ -122,35 +127,48 @@ class Numbers:
                 self._data_dict[current_index],
             )
         )
-        current_index_a = index_iterator.__next__()
-        self._data_dict[current_index_a] = temp_row_list
-        self.score(current_index_a)
+        if temp_row_list != self._data_dict[current_index]:
+            current_index_a = index_iterator.__next__()
+            self._data_dict[current_index_a] = temp_row_list
+            self.score(current_index_a)
+            return True
+        return False
 
-    def moving_up(self):
+    def moving_up(self) -> bool:
         current_index = index_iterator.__current__()
         matrix = [
             [row[i] for row in self._data_dict[current_index]]
             for i in range(len(self._data_dict[current_index][0]))
         ]
-        temp_row_list = list(map(self.moving, matrix))
-        matrix = [
-            [row[i] for row in temp_row_list] for i in range(len(temp_row_list[0]))
-        ]
-        current_index_a = index_iterator.__next__()
-        self._data_dict[current_index_a] = matrix
-        self.score(current_index_a)
+        movealbe_check_result = set(map(lambda item: self.moveable_and_Loose_checker(item)[0], matrix))
+        print(movealbe_check_result)
+        if True in movealbe_check_result:
+            temp_row_list = list(map(self.moving, matrix))
+            matrix = [
+                [row[i] for row in temp_row_list] for i in range(len(temp_row_list[0]))
+            ]
+            current_index_a = index_iterator.__next__()
+            self._data_dict[current_index_a] = matrix
+            self.score(current_index_a)
+            return True
+        return False
 
-    def moving_down(self):
+    def moving_down(self) -> bool:
         current_index = index_iterator.__current__()
         matrix = list(zip(*self._data_dict[current_index]))
         matrix = [list(item)[::-1] for item in matrix]
-        temp_row_list = list(map(self.moving, matrix))
-        matrix = [item[::-1] for item in temp_row_list]
-        matrix = list(zip(*matrix))
-        matrix = [list(item) for item in matrix]
-        current_index_a = index_iterator.__next__()
-        self._data_dict[current_index_a] = matrix
-        self.score(current_index_a)
+        movealbe_check_result = set(map(lambda item: self.moveable_and_Loose_checker(item)[0], matrix))
+        print(movealbe_check_result)
+        if True in movealbe_check_result:
+            temp_row_list = list(map(self.moving, matrix))
+            matrix = [item[::-1] for item in temp_row_list]
+            matrix = list(zip(*matrix))
+            matrix = [list(item) for item in matrix]
+            current_index_a = index_iterator.__next__()
+            self._data_dict[current_index_a] = matrix
+            self.score(current_index_a)
+            return True
+        return False
     
     def reverse(self):
         current_index = index_iterator.__previous__()
@@ -166,15 +184,65 @@ class Numbers:
         current_score = self._score_dict[current_index]
         return current_score
     
+    def moveable_and_Loose_checker(self,current_data_row) -> tuple[bool,bool]:
+        temp_number = 0
+        none_count = 0
+        combine_number = False
+        moveable = False
+        slot_full = False
+        for i in current_data_row:
+            if temp_number == i and i != None:
+                combine_number = True
+                break
+            elif i is None:
+                none_count += 1
+            else:
+                if none_count != 0:
+                    moveable = True
+            if i != None:
+                temp_number = i
+        if  combine_number is True:
+            moveable = True
+        else:
+            if none_count == 0:
+                slot_full = True
+        return moveable, slot_full
+
+    def reset_games(self):
+        self.__init__()
+        player1.__init__()
+    
+    def win_lose_moving_checking(self) -> None:
+        row_checked_set = set()
+        column_checked_set = set()
+        current_numbers_data = self.get_current_data()
+        matrix_current_numbers_data = [
+            [row[i] for row in current_numbers_data]
+            for i in range(len(current_numbers_data))
+        ]
+        # list(map(lambda item: row_checked_set.add(self.moveable_checker(item)[1]), current_numbers_data))
+        for item in current_numbers_data:
+            row_checked_set.add(self.moveable_and_Loose_checker(item)[1])
+        for item in matrix_current_numbers_data:
+            column_checked_set.add(self.moveable_and_Loose_checker(item)[1])
+        if False in row_checked_set or False in column_checked_set:
+            pass
+        else:
+            player1.win = Player.State.Loose
+        return
 playing = Numbers()
 # generater_read = Numbers()
 
 # a = Numbers()
 # a.random_generator()
-# # a.moving_left()
-# # a.moving_right()
-# # a.moving_up()
-# # a.moving_down()
+# # # a.moving_left()
+# a.win_lose_moving_checking
+# a.moving_right()
+# # # a.moving_up()
+# # # a.moving_down()
+
 # print(a._data_dict)
-# c = a.get_current_data()
-# print(c)
+# a.win_lose_moving_checking
+# print(player1.win)
+# # c = a.get_current_data()
+# # print(c)
